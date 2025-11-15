b62fc1ca3c39862036bb016645c5846316254eeb
Edward Ly <contact@edward.ly>
2025-11-14 17:32:23 -0800

    perf: add lazy loading to app config values
    This also replaces the deprecated methods in `OCP\IConfig` with their
    equivalents in `OCP\IAppConfig` in the process.
    
    Signed-off-by: Edward Ly <contact@edward.ly>

--- lib/Migration/Version2000Date20241009004002.php
+++ lib/Migration/Version2000Date20241009004002.php
@@ -11,7 +11,7 @@ namespace OCA\DocuSign\Migration;
 
 use Closure;
 use OCA\DocuSign\AppInfo\Application;
-use OCP\IConfig;
+use OCP\IAppConfig;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Security\ICrypto;
@@ -20,7 +20,7 @@ class Version2000Date20241009004002 extends SimpleMigrationStep {
 
 	public function __construct(
 		private ICrypto $crypto,
-		private IConfig $config,
+		private IAppConfig $appConfig,
 	) {
 	}
 
@@ -31,10 +31,10 @@ class Version2000Date20241009004002 extends SimpleMigrationStep {
 	 */
 	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
 		foreach (['docusign_client_id', 'docusign_token', 'docusign_refresh_token'] as $key) {
-			$value = $this->config->getAppValue(Application::APP_ID, $key);
+			$value = $this->appConfig->getValueString(Application::APP_ID, $key, lazy: true);
 			if ($value !== '') {
 				$encryptedValue = $this->crypto->encrypt($value);
-				$this->config->setAppValue(Application::APP_ID, $key, $encryptedValue);
+				$this->appConfig->setValueString(Application::APP_ID, $key, $encryptedValue, lazy: true);
 			}
 		}
 	}
